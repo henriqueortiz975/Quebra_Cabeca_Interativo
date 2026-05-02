a32e6066a1b4d795806cf4cf768b2b3db3be987f
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-05-02 15:35:12 -0300

    Atualizando tudo

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 //capturar os elementos por ID
 
 //peça 1
-const botaoAtivar = document.getElementById("btnAtivar");
-const peca = document.getElementById("peca");
-const statusTexto = document.getElementById("statusTexto");
-const iconePeca = document.getElementById("iconePeca");
-const tituloPeca = document.getElementById("tituloPeca");
-const textoPeca = document.getElementById("textoPeca");
-
+const statusTexto1 = document.getElementById("statusTexto1");
+const peca1 = document.getElementById("peca1");
+const textoPeca1 = document.getElementById("textoPeca1");
+ 
 //peça 2
-const botaoAtivar2 = document.getElementById("btnAtivar2");
-const peca2 = document.getElementById("peca2");
 const statusTexto2 = document.getElementById("statusTexto2");
-const iconePeca2 = document.getElementById("iconePeca2");
-const tituloPeca2 = document.getElementById("tituloPeca2");
-const textoPeca2 = document.getElementById("textoPeca2");
 
 //Estado da aplicação
-
 let pecaAtivada = false;
 let pecaAtivada2 = false;
 
@@ -25,100 +16,30 @@ let pecaAtivada2 = false;
 
 //vamos adicionar evento de clique na peça 1
 
-botaoAtivar.addEventListener("click", function(){
+peca1.addEventListener("click", function(){
     // Verificar se a peça 1 foi clicada
     if(pecaAtivada === false){
         //remoção de clique "bloqueado"
-        peca.classList.remove("bloqueada");
+        peca1.classList.remove("bloqueada");
 
         //Adicionar classe ativa 
-        peca.classList.add("ativa");
+        peca1.classList.add("ativa");
 
         //Mudança de texto
-        statusTexto.innerText = "Desbloqueado";
+        statusTexto1.innerText = "Desbloqueado";
 
         //Alterar cor do texto
-        statusTexto.style.color = "#22c55e";
+        statusTexto1.style.color = "#22c55e";
 
-        //Altera icone da peça
-        iconePeca.innerText = "";
-
-        //Alterar titulo da peça
-        tituloPeca.innerText = "Peça Ativada"
-
-        //Alterar descrição da peça
-        textoPeca.innerText = "Parabéns! Você ativou a primeira peça";
-
-        //Alterar texto do botão
-        botaoAtivar.innerText = "Peça Ativada";
-        
-        //Desativar o botão após clique
-        botaoAtivar.disabled = true;
+        //Alterar texto
+        textoPeca1.innerText = "parabéns você ativou a primeira peça, clique novamente para abrir o conteúdo de JavaScript e DOM.";
 
         //Atualizar var de controle
         pecaAtivada = true;
-    }
-})
-
-//Fazer a peça 1 ativada ser clicavel
-peca.addEventListener("click", function(){
-    //Verificando se a peça 1 já foi clicada anteriomente
-    if(pecaAtivada === true){
-        //Abrindo uma pagina que fala sobre JavaScript e DOM
+    }else{
+        // Se já está ativada ela abre a página
         window.open(URL = "pagina.html");
     }
 })
 
-botaoAtivar2.addEventListener("click", function(){
-    // Verificar se a peça 1 foi clicada
-    if(pecaAtivada2 === false){
-        //remoção de clique "bloqueado"
-        peca2.classList.remove("bloqueada");
 
-        //Adicionar classe ativa 
-        peca2.classList.add("ativa");
-
-        //Trocar a cor da peça 2
-        peca2.style.background = "#c52222"
-
-        //Mudança de texto
-        statusTexto2.innerText = "Desbloqueado";
-
-        //Alterar cor do texto
-        statusTexto2.style.color = "#22c55e";
-
-        //Altera icone da peça
-        iconePeca2.innerText = "🔴";
-
-        //alterar cor do 
-
-        //Alterar titulo da peça
-        tituloPeca2.innerText = "Peça 2 Ativada"
-
-        //Alterar descrição da peça
-        textoPeca2.innerText = "Parabéns! Você ativou a segunda peça";
-
-        //Alterar texto do botão
-        botaoAtivar2.innerText = "Peça 2 Ativada";
-        
-        //Desativar o botão após clique
-        botaoAtivar2.disabled = true;
-
-        //Atualizar var de controle
-        pecaAtivada2 = true;
-    }
-})
-
-peca2.addEventListener("click", function () {
-    let ativo = peca2.classList.toggle("ativa");
-
-    if (ativo === false) {
-        ativo = ("ativo");
-        peca2.style.background = "#00c046";
-        iconePeca2.innerText = "🟢";
-    } else {
-        ativo =("desativa");
-        peca2.style.background = "#c52222";
-        iconePeca2.innerText = "🔴";
-    }
-});
