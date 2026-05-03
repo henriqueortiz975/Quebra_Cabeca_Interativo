086d4a40255810210da7db8bfa98e5f6a17ce0fe
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-05-02 23:11:52 -0300

    Adicionando mais codigos em HTML, CSS E JavaScript

--- js/script.js
+++ js/script.js
@@ -11,10 +11,17 @@ pecas.forEach(function(peca) {
     peca.classList.remove("bloqueada");
 
     status.innerText = "Desbloqueado";
-    status.style.color = "#22c55e";
+    status.style.color = "#f1f1f1";
 
     texto.innerText = "Parabéns! Você ativou essa peça.";
 
+    if(peca.classList.contains("ativa")){
+        const peca1 = document.getElementById("peca1");
+        peca1.addEventListener("click", function(){
+            window.open(URL = "pagina.html");
+        });
+    }
+    
   });
 
 });
